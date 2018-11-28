39fcd23ba42a80ae36aa614592383653ad0f45d3
Jonathan Booher <jaustinb@stanford.edu>
2018-11-28 09:58:14 -0800

    add support for images from trained

--- datasets/split_data.py
+++ datasets/split_data.py
@@ -7,6 +7,7 @@ from random import shuffle
 parser = argparse.ArgumentParser()
 parser.add_argument('--path_A', default='./orig', help='Path to the original data')
 parser.add_argument('--path_B', default='./modi', help='Path to the modified data')
+parser.add_argument('--path_trained', default='./trained', help='path to images from the trained policy')
 parser.add_argument('--output_dir', default='sawyer', help='Path the output directory for the data')
 parser.add_argument('--splits_A', nargs='+',   default=[90, 0, 10], help='The splits for the data defined as X Y Z    ' \
                     'where X is the percent train, Y is the percent valid and Z is the percent test')
@@ -27,9 +28,11 @@ if __name__ == '__main__':
 
     files_A = glob.glob('{}/*.png'.format(args.path_A))
     files_B = glob.glob('{}/*.png'.format(args.path_B))
+    files_trained = glob.glob('{}/*.png'.format(args.path_trained))
 
     shuffle(files_A)
     shuffle(files_B)
+    shuffle(files_trained)
 
     if not os.path.isdir(args.output_dir):
         os.mkdir('./{}'.format(args.output_dir))
@@ -39,17 +42,23 @@ if __name__ == '__main__':
             os.mkdir('./{}/{}'.format(args.output_dir, subdir))
 
 
-    len_train = (int(len(files_A)*args.splits_A[0]/100.), int(len(files_B)*args.splits_B[0]/100.))
-    len_valid = (int(len(files_A)*args.splits_A[1]/100.), int(len(files_B)*args.splits_B[1]/100.))
-    len_test = (int(len(files_A)*args.splits_A[2]/100.), int(len(files_B)*args.splits_B[2]/100.))
+    len_train = (int(len(files_A)*args.splits_A[0]/100.),
+                 int(len(files_B)*args.splits_B[0]/100.),
+                 int(len(files_trained)*args.splits_A[0]/100.))
+    len_valid = (int(len(files_A)*args.splits_A[1]/100.),
+                 int(len(files_B)*args.splits_B[1]/100.),
+                 int(len(files_trained)*args.splits_A[1]/100.))
+    len_test = (int(len(files_A)*args.splits_A[2]/100.),
+                int(len(files_B)*args.splits_B[2]/100.),
+                int(len(files_trained)*args.splits_A[2]/100.))
 
-    train_A, train_B = files_A[:len_train[0]], files_B[:len_train[1]]
-    files_A, files_B = files_A[len_train[0]:], files_B[len_train[1]:]
+    train_A, train_B, train_trained = files_A[:len_train[0]], files_B[:len_train[1]], files_trained[:len_train[2]]
+    files_A, files_B, files_trained = files_A[len_train[0]:], files_B[len_train[1]:], files_trained[len_train[2]:]
 
-    valid_A, valid_B = files_A[:len_valid[0]], files_B[:len_valid[1]]
-    files_A, files_B = files_A[len_valid[0]:], files_B[len_valid[1]:]
+    valid_A, valid_B, valid_trained = files_A[:len_valid[0]], files_B[:len_valid[1]], files_trained[:len_valid[2]]
+    files_A, files_B, files_trained = files_A[len_valid[0]:], files_B[len_valid[1]:], files_trained[len_valid[2]:]
 
-    test_A, test_B = files_A, files_B
+    test_A, test_B, test_trained = files_A, files_B, files_trained
 
 
     for f in train_A:
@@ -64,3 +73,13 @@ if __name__ == '__main__':
         os.system('cp {} ./{}/testA'.format(f, args.output_dir))
     for f in test_B:
         os.system('cp {} ./{}/testB'.format(f, args.output_dir))
+
+    for f in train_trained:
+        f_name = f.split('/')[-1]
+        os.system('cp {} ./{}/trainA/trained_{}'.format(f, args.output_dir, f_name))
+    for f in valid_trained:
+        f_name = f.split('/')[-1]
+        os.system('cp {} ./{}/valA/trained_{}'.format(f, args.output_dir, f_name))
+    for f in test_trained:
+        f_name = f.split('/')[-1]
+        os.system('cp {} ./{}/testA/trained_{}'.format(f, args.output_dir, f_name))
