cb1c95b6c582a480523da2e7b1d7e1be39407c40
Jonathan Booher <jaustinb@stanford.edu>
2018-12-09 01:27:09 -0800

    improved data generation scripts

---
+++ data_generator.py
@@ -0,0 +1,208 @@
+import os
+from surreal.env import *
+from surreal.main.ppo_configs import *
+from PIL import Image
+import matplotlib.pyplot as plt
+from options.datagen_options import DataGenOptions
+from mujoco_py import MjSimState
+import pickle
+
+def restore_env(env_config, size, internal=False):
+    """
+    Restore the env
+    """
+    env_config.eval_mode.render = True
+    env, env_config = make_env(env_config, 'eval', internal=internal)
+    env.unwrapped.camera_width, env.unwrapped.camera_height = size, size
+    return env, env_config
+
+def restore_agent(learner_config, env_config, session_config, model):
+    """
+    Restores an agent from a model.
+    """
+    learner_config.algo.use_z_filter = False
+    agent_class = PPOAgent
+    agent = agent_class(
+        learner_config=learner_config,
+        env_config=env_config,
+        session_config=session_config,
+        agent_id=0,
+        agent_mode='eval_deterministic_local',
+    )
+
+    agent.model.load_state_dict(model)
+    return agent
+
+def restore_model(path_to_ckpt):
+    """
+    Loads model from a ckpt file.
+    """
+    with open(path_to_ckpt, 'rb') as fp:
+        data = pickle.load(fp)
+    return data['model']
+
+
+def rollout_save_states(opt, env, env_config):
+    # retrieve the policy params if necessary and restore the model
+    print("\nLoading policy located at {}\n".format(opt.policy_path))
+    model = restore_model(opt.policy_path)
+
+    env.unwrapped.camera_width, env.unwrapped.camera_height = 84, 84
+
+    # restore the configs
+    session_config, learner_config = PPO_DEFAULT_SESSION_CONFIG, PPO_DEFAULT_LEARNER_CONFIG
+
+    session_config.agent.num_gpus = 0
+    session_config.learner.num_gpus = 0
+
+    agent = restore_agent(learner_config, env_config, session_config, model)
+    print("Successfully loaded agent and model!")
+
+    if not opt.save_obs:
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+
+    states = []
+    for rollout in range(opt.n_rollouts):
+        ob, _ = env.reset()
+        for step in range(opt.n_steps):
+            obs = env.unwrapped._get_observation()["image"]
+
+            if not opt.save_obs:
+                plt.imshow(obs[::-1])
+                ax.set_title('Observation')
+                plt.pause(0.01)
+                plt.draw()
+
+            state = env.unwrapped.sim.get_state().flatten().copy()
+            states.append(state)
+
+            ob['pixel']['camera0'] = np.transpose(obs, (2,0,1))
+
+
+            action = agent.act(ob)
+            ob, _, _, _ = env.step(action)
+
+    print()
+    np.save(opt.states_file, np.array(states))
+
+def rollout_from_state(opt, env):
+
+    t = 'textured' if opt.internal else 'normal'
+    if opt.save_obs:
+        os.makedirs(opt.data_root + '/' + opt.dataset_name + '/state_rollout_{}'.format(t), exist_ok=True)
+
+    save_path = opt.data_root + '/' + opt.dataset_name + '/state_rollout_{}'.format(t)
+
+    states = np.load(opt.states_file + '.npy')
+    env.reset()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    for state_index in range(states.shape[0]):
+        state = MjSimState.from_flattened(states[state_index], env.unwrapped.sim)
+        env.unwrapped.sim.set_state(state)
+        env.unwrapped.sim.forward()
+        obs = env.unwrapped._get_observation()['image'][::-1]
+        obs = Image.fromarray(obs).convert('RGB')
+
+        if opt.save_obs:
+            obs.save(save_path + '/from_states_{}.jpg'.format(state_index))
+        else:
+            plt.imshow(obs)
+            ax.set_title('Observation')
+            plt.pause(0.01)
+            plt.draw()
+
+def rollout_policy(opt, env, env_config):
+
+    if opt.save_obs:
+        os.makedirs(opt.data_root + '/' + opt.dataset_name + '/rollout', exist_ok=True)
+
+    save_path = opt.data_root + '/' + opt.dataset_name + '/rollout'
+    # retrieve the policy params if necessary and restore the model
+    print("\nLoading policy located at {}\n".format(opt.policy_path))
+    model = restore_model(opt.policy_path)
+
+    env.unwrapped.camera_width, env.unwrapped.camera_height = 84, 84
+
+    # restore the configs
+    session_config, learner_config = PPO_DEFAULT_SESSION_CONFIG, PPO_DEFAULT_LEARNER_CONFIG
+
+    session_config.agent.num_gpus = 0
+    session_config.learner.num_gpus = 0
+
+    agent = restore_agent(learner_config, env_config, session_config, model)
+    print("Successfully loaded agent and model!")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    total_reward = 0
+    for rollout in range(opt.n_rollouts):
+        ob, _ = env.reset()
+        for step in range(opt.n_steps):
+            obs = env.unwrapped._get_observation()["image"]
+
+            ob['pixel']['camera0'] = np.transpose(obs, (2,0,1))
+            action = agent.act(ob)
+            ob, r, done, _ = env.step(action)
+
+            total_reward += r
+
+            if opt.save_obs:
+                o = Image.fromarray(obs[::-1]).resize((opt.size, opt.size))
+                o.save(save_path + '/rollout_{}_{}.jpg'.format(rollout, step))
+            else:
+                plt.imshow(obs)
+                ax.set_title('Observation')
+                plt.pause(0.01)
+                plt.draw()
+
+            if done:
+                env.reset()
+
+    print('Average return:   {}'.format(total_reward / (opt.n_rollouts * opt.n_steps)))
+
+def rollout_random(opt, env):
+
+    t = 'textured' if opt.internal else 'normal'
+    if opt.save_obs:
+        os.makedirs(opt.data_root + '/' + opt.dataset_name + '/random_{}'.format(t), exist_ok=True)
+
+    save_path = opt.data_root + '/' + opt.dataset_name + '/random_{}'.format(t)
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    for rollout in range(opt.n_rollouts):
+        env.reset()
+        for step in range(opt.n_steps):
+            obs = env.unwrapped._get_observation()['image'][::-1]
+            obs = Image.fromarray(obs).convert('RGB')
+
+            if opt.save_obs:
+                obs.save(save_path + '/random_{}_{}.jpg'.format(rollout, step))
+            else:
+                plt.imshow(obs)
+                ax.set_title('Observation')
+                plt.pause(0.01)
+                plt.draw()
+
+            action = np.random.randn(env.unwrapped.dof)
+            env.step(action)
+
+
+if __name__ == '__main__':
+    options = DataGenOptions().parse()
+
+    environment_config = PPO_DEFAULT_ENV_CONFIG
+    environment, _ = restore_env(environment_config, options.size, internal=options.internal)
+
+
+    if options.mode == 'save_states':
+        rollout_save_states(options, environment, environment_config)
+    elif options.mode == 'state2im':
+        rollout_from_state(options, environment)
+    elif options.mode == 'rollout':
+        rollout_policy(options, environment, environment_config)
+    elif options.mode == 'random':
+        rollout_random(options, environment)
---
+++ options/datagen_options.py
@@ -0,0 +1,28 @@
+from .base_options import BaseOptions
+
+
+class DataGenOptions(BaseOptions):
+    def initialize(self, parser):
+        parser = BaseOptions.initialize(self, parser)
+        #  Dropout and Batchnorm has different behavioir during training and test.
+        parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
+        parser.add_argument('--policy_path', type=str,
+                            default="/home/jonathan/Downloads/learner.24000.ckpt.cpu",
+                            help='path to the surreal weights file')
+        parser.add_argument('--save_obs', action='store_true', help='if we should save the image obs from the rollout')
+        parser.add_argument('--dataset-name', type=str, default='sawyer_gen')
+        parser.add_argument('--data_root', type=str, default='./datasets')
+        parser.add_argument('--states-file', type=str, default='states')
+        parser.add_argument('--internal', action='store_true', help='should we use robosuite internal')
+        parser.add_argument('--n_rollouts', type=int, default=100, help='the number of rollouts we should perform')
+        parser.add_argument('--n_steps', type=int, default=200, help='the number of steps per rollout we should perform')
+
+        parser.add_argument('--mode', type=str, default='rollout', help='the mode of data generation we should run')
+        parser.add_argument('--size', type=int, default=256, help='size of images we should make')
+
+        parser.set_defaults(model='test')
+
+        # To avoid cropping, the loadSize should be the same as fineSize
+        parser.set_defaults(loadSize=parser.get_default('fineSize'))
+        self.isTrain = False
+        return parser
